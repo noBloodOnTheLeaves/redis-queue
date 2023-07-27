824dfc39199ffd8f96728ad61910885748db16de
Igor <igor-skromnik@yandex.ru>
2023-07-27 19:51:27 +0300

    add-auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Row\RowController;
+use App\Http\Controllers\Api\Upload\XlsController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +18,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class,'authenticate']);
+Route::post('/register', [AuthController::class,'register']);
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+
+    Route::get('/auth/user', function (Request $request) {
+        return ['data' => $request->user()];
+    });
+
+    Route::group(['prefix' => 'upload'], function () {
+        Route::post('/file_rows', [XlsController::class,'upload']);
+    });
+
+    Route::get('/batch/{batchId}', function (string $batchId) {
+        return Bus::findBatch($batchId);
+    });
+
+    Route::group(['prefix' => 'rows'], function () {
+        Route::get('/', [RowController::class,'show']);
+    });
+
+    Route::delete('/logout', [AuthController::class,'logout']);
 });
